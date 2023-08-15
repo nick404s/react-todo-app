cb5c8f9fdba8f6b20a6f11005d27d69cd23ce914
Nikolay Skryabin <nickskryabin@gmail.com>
2023-08-14 20:05:18 -0700

    add line through text decoration for the checked todos

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,87 +1,79 @@
 import {
-    ListItem, 
-    ListItemButton, 
-    ListItemIcon, 
-    Checkbox, 
-    IconButton,
-    Divider,
-    TextField
-} from '@mui/material';
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  Checkbox,
+  IconButton,
+  Divider,
+  TextField,
+} from "@mui/material";
 
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import EditIcon from '@mui/icons-material/Edit';
-import { useState } from 'react';
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
 
+export default function TodoItem({ todo, removeItem, toggleItem, editItem }) {
+  const { id, title, completed } = todo;
 
-export default function TodoItem({ todo, removeItem, toggleItem, editItem }){
+  const labelId = `checkbox-list-label-${id}`;
 
-    const {id, title, completed} = todo;
+  const [text, setText] = useState(title);
 
-    const labelId = `checkbox-list-label-${id}`;
+  const [isDisabled, setIsDisabled] = useState(true);
 
-    const [text, setText] = useState(title);
+  // change text
+  const handleChange = (evt) => {
+    const value = evt.target.value;
 
-    const [isDisabled, setIsDisabled] = useState(true);
-
-
-    // change text
-    const handleChange = (evt) => {
-        const value = evt.target.value;
-
-        if (value) {
-            setText(value);
-        }
-        
-    };
-
-    // save text
-    const handleSave = () => {
-        editItem(id, text);
-        setIsDisabled(true);
-    };
-
-    // unblock input
-    const handleDisable = () => {
-        setIsDisabled(false);
+    if (value) {
+      setText(value);
     }
+  };
 
+  // save text
+  const handleSave = () => {
+    editItem(id, text);
+    setIsDisabled(true);
+  };
 
-    return (
-          <ListItem 
-            key={id}>            
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  onChange={toggleItem}
-                />
-              </ListItemIcon>
-                <TextField fullWidth
-                    variant='standard'
-                    id={labelId} 
-                    value={text} 
-                    disabled={isDisabled} 
-                    onChange={handleChange} 
-                    onBlur={handleSave} 
-                />
-            </ListItemButton>
-            
-            <IconButton  onClick={handleDisable}>
-                <EditIcon/>
-            </IconButton>
+  // unblock input
+  const handleDisable = () => {
+    setIsDisabled(false);
+  };
 
-            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-            <IconButton 
-                edge="end" 
-                aria-label="comments" 
-                onClick={removeItem}
-            >
-                <DeleteForeverIcon />
-            </IconButton>      
-          </ListItem>
-    );  
+  return (
+    <ListItem key={id}>
+      <ListItemButton role={undefined} dense>
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            checked={completed}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ "aria-labelledby": labelId }}
+            onChange={toggleItem}
+          />
+        </ListItemIcon>
+        <TextField
+          fullWidth
+          variant="standard"
+          id={labelId}
+          style={{ textDecoration: completed ? "line-through" : "none" }}
+          value={text}
+          disabled={isDisabled}
+          onChange={handleChange}
+          onBlur={handleSave}
+        />
+      </ListItemButton>
+
+      <IconButton onClick={handleDisable}>
+        <EditIcon />
+      </IconButton>
+
+      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      <IconButton edge="end" aria-label="comments" onClick={removeItem}>
+        <DeleteForeverIcon />
+      </IconButton>
+    </ListItem>
+  );
 }
